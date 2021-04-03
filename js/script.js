202f4ed10f97debeae1f00114d9ae380d3de9bf1
Thies <Thiesvanderzon@gmail.com>
2021-04-03 12:13:34 +0200

    gewerkt aan styling en licht vak plaatje switcht maar staat nog niet goed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 function show() {
     document.getElementById('sidebar').classList.toggle('active');
 }
+let image_tracker = 'lu';
+function change(){
+    let image = document.getElementById('lamp-uit');
+    if(image_tracker == 'lu'){
+        image.src = '/Images/lamp-aan.png';
+        image_tracker = 'la';
+    }
+    else{
+        image.src = '/Images/lamp-uit.png';
+        image_tracker = 'lu';
+    }
+}
